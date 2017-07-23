e5919e9d180c90ac3aa8aa5cd3d3f8220f90b5b1
bother7 <github email address>
2017-07-23 13:51:31 -0400

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,30 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+
+attr_accessor :search, :array
+
+
+  def initialize (search)
+    @search = search.split("").sort
+  end
+
+  def match (array)
+    @array = array
+    x = @array.select do |word|
+      y = word.split("")
+      # binding.pry
+      if y.sort == @search
+        # binding.pry
+        word
+      end
+    end
+    x
+    # binding.pry
+  end
+
+
+
+
+end
